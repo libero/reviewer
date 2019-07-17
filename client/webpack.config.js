f727996fd8821ba72ea055ec2b6ff28dee89f8b0
Erez Odier <erez.odier@gmail.com>
2019-07-17 10:42:20 +0100

    added hot module reloading and example fetch

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,21 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: './index.js',
+  devServer: {
+    stats: "errors-only",
+    host: process.env.HOST,
+    port: process.env.PORT,
+    open: true,
+    overlay: true,
+    hot: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        pathRewrite: {'^/api' : ''},
+        changeOrigin: true,
+      }
+    }
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
