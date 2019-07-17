2fd1d118fe276f999111cc9e9bb44b6b6c65808e
Cory <c.donkin@elifesciences.org>
2019-07-17 11:21:32 +0100

    seperate webpack config for build and start

--- client/webpack.development.config.js
+++ client/webpack.development.config.js
@@ -3,6 +3,21 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: './index.js',
+  devServer: {
+    stats: "errors-only",
+    host: process.env.HOST,
+    port: 9000,
+    open: true,
+    overlay: true,
+    hot: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        pathRewrite: {'^/api' : ''},
+        changeOrigin: true,
+      }
+    }
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
