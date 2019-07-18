424d73bf835c48488e3f3dfd86ea02582d10351d
Cory <c.donkin@elifesciences.org>
2019-07-18 10:08:56 +0100

    set up routing to be compatible with derver hosted file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react'
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
-const App = () => {
-  const [cat, setCat] = React.useState('')
-  const fetchFromServer = () => window.fetch('http://localhost:9000/api/cats')
-  .then(response => response.json())
-  .then(json => setCat(json.name))
-
-  return (
-  <div>
-    <button onClick={fetchFromServer}>Whats the cats name?</button><br></br>
-    <span>cat name is {cat}</span><br></br>
-  </div>
-  )
-}
+const App: React.FunctionComponent = () => (
+  <Router>
+      <Route exact path="/" component={() => <div>home route</div>} />
+  </Router>
+)
 
 export default App
