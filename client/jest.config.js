2a27a49fd5e934c433c88ab56090af9fda06ef23
Cory <c.donkin@elifesciences.org>
2019-07-24 11:58:54 +0100

    set up test config to transform asset and styling imports. Fixes broken tests

--- client/jest.config.js
+++ client/jest.config.js
@@ -3,4 +3,8 @@ module.exports = {
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test-utils/fileTransform.js",
+        "\\.(css|scss)$": "<rootDir>/test-utils/styleTransform.js"
+      }
 };
---
+++ client/test-utils/fileTransform.js
@@ -0,0 +1,8 @@
+// Used to transform asset imports, required when runing tests: https://jestjs.io/docs/en/webpack#mocking-css-modules
+const path = require('path');
+
+module.exports = {
+  process(src, filename, config, options) {
+    return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
+  },
+};
