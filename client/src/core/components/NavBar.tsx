8afcfd777d17c513bbf28cedea87c1f95f50f78f
Cory <c.donkin@elifesciences.org>
2019-07-26 11:45:06 +0100

    linting fixes for client

--- client/src/core/components/NavBar.tsx
+++ client/src/core/components/NavBar.tsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import { AppBar, AppBarIcon, NavMenu } from '../../ui/atoms'
+import React from 'react';
+import { AppBar, AppBarIcon, NavMenu } from '../../ui/atoms';
 import Logo from '../assets/elife-logo.png';
 
 const NavBar: React.FC = (): JSX.Element => (
-  <AppBar>
-    <AppBarIcon imgSrc={Logo} link="/" altText="eLife logo"/>
-    <NavMenu items={[
-      {
-        display: 'Dashboard',
-        url: '/'
-      },
-      {
-        display: 'Author guide',
-        url: '/author-guide'
-      },
-      {
-        display: 'Reviewer guide',
-        url: '/reviewer-guide'
-      },
-      {
-        display: 'Contact us',
-        url: '/contact-us'
-      }
-    ]}/>
-    App Bar
-  </AppBar>
-)
+    <AppBar>
+        <AppBarIcon imgSrc={Logo} link="/" altText="eLife logo" />
+        <NavMenu
+            items={[
+                {
+                    display: 'Dashboard',
+                    url: '/',
+                },
+                {
+                    display: 'Author guide',
+                    url: '/author-guide',
+                },
+                {
+                    display: 'Reviewer guide',
+                    url: '/reviewer-guide',
+                },
+                {
+                    display: 'Contact us',
+                    url: '/contact-us',
+                },
+            ]}
+        />
+        App Bar
+    </AppBar>
+);
 
-export default NavBar
+export default NavBar;
