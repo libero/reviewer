40e69d5c32496acece49028f0c8d87cfad2a082e
Cory <c.donkin@elifesciences.org>
2019-07-26 13:22:46 +0100

    fix linting part 2

--- client/src/dashboard/components/Dashboard.tsx
+++ client/src/dashboard/components/Dashboard.tsx
@@ -4,7 +4,11 @@ import { getSubmissions, startSubmission } from '../../submission/submission.ent
 import { Submission } from '../../submission/types';
 import moment from 'moment';
 
-const SubmissionEntry = ({ submission }: { submission: Submission }) => {
+interface Props {
+    submission: Submission;
+}
+
+const SubmissionEntry: React.FC<Props> = ({ submission }: Props): JSX.Element => {
     const submissionTimeStamp: Date = new Date();
     submissionTimeStamp.setTime(Number.parseInt(submission.updated));
     return (
@@ -31,50 +35,54 @@ const SubmissionEntry = ({ submission }: { submission: Submission }) => {
     );
 };
 
-const Dashboard = withRouter(({ history }) => {
-    const [submissions, setSubmissions] = useState([]);
-    const [fetched, setFetched] = useState(false);
+const Dashboard = withRouter(
+    ({ history }): JSX.Element => {
+        const [submissions, setSubmissions] = useState([]);
+        const [fetched, setFetched] = useState(false);
 
-    // only fetch once to prevent render loop
-    if (!fetched) {
-        getSubmissions()
-            .then((fetchedSubmissions: Submission[]): void => {
-                setSubmissions(fetchedSubmissions);
-                setFetched(true);
-            })
-            .catch((): void => {
-                setFetched(true);
-            });
-    }
+        // only fetch once to prevent render loop
+        if (!fetched) {
+            getSubmissions()
+                .then((fetchedSubmissions: Submission[]): void => {
+                    setSubmissions(fetchedSubmissions);
+                    setFetched(true);
+                })
+                .catch((): void => {
+                    setFetched(true);
+                });
+        }
 
-    return (
-        <div className="dashboard">
-            <button
-                onClick={(): void => {
-                    startSubmission().then((submission): void => {
-                        history.push(`/submission/${submission.id}/title`);
-                    });
-                }}
-            >
-                New Submission
-            </button>
-            <h2 style={{ paddingTop: '16px', paddingBottom: '32px' }}>Submissions</h2>
-            <ul style={{ paddingLeft: 0 }}>
-                {submissions.length === 0 ? (
-                    <div>You don{"'"}t have any submissions. Maybe you should make one?</div>
-                ) : (
-                    submissions.reverse().map((sub, index): unknown => <SubmissionEntry key={index} submission={sub} />)
-                )}
-            </ul>
-            <div style={{ textAlign: 'center', color: 'rgb(136, 136, 136)' }}>
-                <p>
-                    To find existing submissions or to submit a <Link to="/author-guide/types">Research Advance</Link>{' '}
-                    please visit our{' '}
-                    <a href="https://submit.elifesciences.org">full peer review and submissions system</a>{' '}
-                </p>
+        return (
+            <div className="dashboard">
+                <button
+                    onClick={(): void => {
+                        startSubmission().then((submission): void => {
+                            history.push(`/submission/${submission.id}/title`);
+                        });
+                    }}
+                >
+                    New Submission
+                </button>
+                <h2 style={{ paddingTop: '16px', paddingBottom: '32px' }}>Submissions</h2>
+                <ul style={{ paddingLeft: 0 }}>
+                    {submissions.length === 0 ? (
+                        <div>You don{"'"}t have any submissions. Maybe you should make one?</div>
+                    ) : (
+                        submissions
+                            .reverse()
+                            .map((sub, index): unknown => <SubmissionEntry key={index} submission={sub} />)
+                    )}
+                </ul>
+                <div style={{ textAlign: 'center', color: 'rgb(136, 136, 136)' }}>
+                    <p>
+                        To find existing submissions or to submit a{' '}
+                        <Link to="/author-guide/types">Research Advance</Link> please visit our{' '}
+                        <a href="https://submit.elifesciences.org">full peer review and submissions system</a>{' '}
+                    </p>
+                </div>
             </div>
-        </div>
-    );
-});
+        );
+    },
+);
 
 export default Dashboard;
