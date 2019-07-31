59a23fe5559aaf9bd6f10879a37bca1dfee4a752
Cory <c.donkin@elifesciences.org>
2019-07-31 15:49:13 +0100

    unit test burger menu

--- client/src/ui/molecules/BurgerMenu.test.tsx
+++ client/src/ui/molecules/BurgerMenu.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, render, RenderResult } from '@testing-library/react';
+import { cleanup, render, RenderResult, fireEvent } from '@testing-library/react';
 import routerWrapper from '../../../test-utils/routerWrapper';
 import BurgerMenu from './BurgerMenu';
 import { MenuItemType } from './Menu'
@@ -17,12 +17,35 @@ describe('NavMenu', (): void => {
   it('should not show menu on mount', (): void => {
     const { container } = renderBurgerMenu();
     expect(container.querySelector('.burger_menu__overlay')).not.toBeInTheDocument()
-  })
+  });
   it('should have aria-haspopup on burger menu button', (): void => {
     const { container } = renderBurgerMenu();
     expect(container.querySelector('.burger_menu__icon_button--expand')).toHaveAttribute('aria-haspopup')
+  });
+  it('shows layover when burger button pressed', (): void => {
+    const { container } = renderBurgerMenu();
+    expect(container.querySelector('.burger_menu__overlay')).not.toBeInTheDocument();
+    fireEvent.click(container.querySelector('.burger_menu__icon_button--expand'));
+    expect(container.querySelector('.burger_menu__overlay')).toBeInTheDocument();
+  });
+  it('collapses when close button pressed', (): void => {
+    const { container } = renderBurgerMenu();
+    fireEvent.click(container.querySelector('.burger_menu__icon_button--expand'));
+    expect(container.querySelector('.burger_menu__overlay')).toBeInTheDocument();
+    fireEvent.click(container.querySelector('.burger_menu__icon_button--collapse'));
+    expect(container.querySelector('.burger_menu__overlay')).not.toBeInTheDocument();
+  });
+  it('displays the menu items passed', () => {
+    const { container, getByText } = renderBurgerMenu([{ url: '/link', display: 'Link' }, { url: '/link2', display: 'Link2'}]);
+    fireEvent.click(container.querySelector('.burger_menu__icon_button--expand'));
+    expect(getByText('Link')).toBeInTheDocument()
+    expect(getByText('Link2')).toBeInTheDocument()
   })
-  it.skip('shows layover when burger button pressed', (): void => {
-   
+  it('collapses when a reletive menu link is clicked', (): void => {
+    const { container, getByText } = renderBurgerMenu([{ url: '/link', display: 'Link' }]);
+    fireEvent.click(container.querySelector('.burger_menu__icon_button--expand'));
+    expect(container.querySelector('.burger_menu__overlay')).toBeInTheDocument();
+    fireEvent.click(getByText('Link'));
+    expect(container.querySelector('.burger_menu__overlay')).not.toBeInTheDocument();
   })
 });
