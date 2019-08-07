b8fc21e059916f931ed862803ed1e7459729a519
Will Byrne <w.byrne@elifesciences.org>
2019-08-07 10:33:58 +0100

    add footer and paragraph components

--- client/src/ui/atoms/index.ts
+++ client/src/ui/atoms/index.ts
@@ -1,4 +1,6 @@
 export { default as AppBar } from './AppBar';
 export { default as AppBarIcon } from './AppBarIcon';
 export { default as Button } from './Button';
+export { default as Footer } from './Footer';
 export { default as Modal } from './Modal';
+export { default as Paragraph } from './Paragraph';
