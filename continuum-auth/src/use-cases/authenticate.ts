19c2cd7c19d842edeca139f20ce8e3af87024229
Peter East <petereast+gpg@pm.me>
2019-09-02 14:44:01 +0100

    Start fleshing out the controller code

--- continuum-auth/src/use-cases/authenticate.ts
+++ continuum-auth/src/use-cases/authenticate.ts
@@ -1,4 +1,21 @@
-// This is the endpoint that does the actual token exchange/user lookup and signing the output token
-//
+import {Request, Response} from 'express';
+import { DomainLogger as logger } from '../logger';
+import { Option } from 'funfix';
+import { encode } from '../jwt';
 
-export const foo = "bar";
+interface AuthenticateParams  {
+  token: string;
+}
+// This is the endpoint that does the actual token exchange/user lookup and signing the output token
+// And yeah, I know the controller/usecase code shouldn't be mixed but idec, we can refactor it at some point
+export const Authenticate = (req: Request, res: Response) => {
+  Option.of(req.params['token']).map((token: string): void => {
+
+    const some_token = {cool: "beans"};
+
+    res.status(200).json({ok: true, token: encode(some_token)});
+  }).getOrElseL(() => {
+    logger.warn("noTokenProvided");
+    res.status(500).json({ok: false});
+  });
+};
--- continuum-auth/src/use-cases/login.ts
+++ continuum-auth/src/use-cases/login.ts
@@ -1,7 +1,14 @@
 // This will basically just redirect the user to the OAuth provider (ORCiD)
 import { Request, Response } from 'express';
+import { DomainLogger as logger } from '../logger';
+
+import config from '../config';
+
+const { auth: { redirect_url } } = config;
 
 export const Login = (req: Request, res: Response) => {
 
-  res.redirect("www.google.com");
+  logger.info("Redirecting request", {redirect_url});
+
+  res.redirect("http://www.google.com");
 };
