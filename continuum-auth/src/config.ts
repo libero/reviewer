3fb5a8455e8ddc39042593635372c77bb149e4d7
Peter East <petereast+gpg@pm.me>
2019-09-03 14:42:10 +0100

    Add a repo for the profiles service

--- continuum-auth/src/config.ts
+++ continuum-auth/src/config.ts
@@ -2,11 +2,20 @@
 
 const config = {
   auth: {
-    redirect_url: "https://www.google.com"
+    // Where the /login route sends you - a.k.a the identity server
+    login_redirect_url: "https://elifesciences.org/submit",
+    // App entry point i.e. the resource server that needs authentication
+    authorised_redirect_url: "http://localhost:3001/login",
   },
-  jwt: {
+  internal_jwt: {
+    // This token is global to libero services
     secret: "super_secret_jam",
-    expiresIn: "24h",
+    expiresIn: "30m",
+  },
+  journal_jwt: {
+    // This secret is used by journal to sign outgoing tokens, and used here to verify those
+    // tokens
+    secret: "some_secret_from_journal",
   },
 };
 
