1cc764626632b98f5e789b5af39254c61c5e72aa
Peter East <petereast+gpg@pm.me>
2019-09-03 14:43:01 +0100

    Continue infra code

--- continuum-auth/src/main.ts
+++ continuum-auth/src/main.ts
@@ -2,6 +2,8 @@ import * as express from 'express';
 import {Express, Request, Response} from 'express';
 import { InfraLogger as logger } from './logger';
 
+import { ProfilesService } from './repo/profiles';
+
 import { HealthCheck, Login, Authenticate } from './use-cases';
 
 function init() {
@@ -10,19 +12,22 @@ function init() {
   const app: Express = express();
 
   // Setup connections to the databases/message queues etc.
+  const profilesConnector = new ProfilesService("https://api.elifesciences.org/profiles");
 
+  // Setup routes
   app.use("/", (req: Request, res: Response, next) => {
     // Maybe this should be trace level logging
     logger.info(`${req.method} ${req.path}`, {} );
     next();
   });
 
-  app.get("/health", HealthCheck);
+  // This is how we do dependency injection at the moment
+  app.get("/health", HealthCheck(undefined));
 
   app.post("/login", Login);
   app.get("/login", Login);
 
-  app.get("/authenticate/:token", Authenticate);
+  app.get("/authenticate/:token", Authenticate(profilesConnector));
   
   return app;
 }
