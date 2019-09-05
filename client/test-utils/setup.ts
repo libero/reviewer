2692e4e4bc61fa14d4d3b701fb3c5f20de26d9c2
Erez Odier <erez.odier@gmail.com>
2019-09-05 11:56:10 +0100

    Made i18n more configurable, different settings for test

--- client/test-utils/setup.ts
+++ client/test-utils/setup.ts
@@ -1,11 +1,18 @@
 // This script is ran before the tests begin.
 
 //required to set global fetch for testing ApolloClient
-require('whatwg-fetch');
-
-require('../src/core/utils/i18n');
-
+import 'whatwg-fetch';
 // give us extra assertion options on jests expect
-require('@testing-library/jest-dom/extend-expect');
+import '@testing-library/jest-dom/extend-expect';
+import I18n from '../src/core/utils/i18n';
+
 const jestDom = require('@testing-library/jest-dom');
 expect.extend({...jestDom});
+
+I18n({
+    debug: false,
+    fallbackLng: false,
+    react: {
+        useSuspense: false
+    }
+})
