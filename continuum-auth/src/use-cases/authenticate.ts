6cc373061b67a81994a6c2986f7bb0e53450049b
Peter East <petereast@users.noreply.github.com>
2019-09-09 10:58:59 +0100

    Add a strawman token interface (#201)
    * Add a strawman token interface
    
    * Generate a valid token in continuum-auth
    
    * lint
    
    * Add decouple from elife
    
    * Fix type error

--- continuum-auth/src/use-cases/authenticate.ts
+++ continuum-auth/src/use-cases/authenticate.ts
@@ -6,16 +6,41 @@ import { ProfilesRepo } from "../repo/profiles";
 import { v4 } from "uuid";
 import config from '../config';
 
+// NOTE: This is a duplicate from server/src/modules/auth/types.ts
+// we just haven't set up a way of having a shared types module
+
+export interface UserIdentity { // don't forget this is merged with the rest of the JWT standard fields
+  token_id: string; // Generated, unique per token
+  token_version: '0.1-alpha'; // Generated, hardcoded
+  identity: { // Unique per user, lookup from profiles, people, user management services
+    user_id: string;
+    external: Array<{
+      id: string;
+      domain: string; // e.g. "elife-profiles", "elife-people", "orcid"
+    }>;
+  };
+
+  // Generic user roles, basically to know which
+  // So a user is associated with a `journal` when they've got a role with it?
+  roles: Array<{
+    journal: string; // e.g. "elife"
+    kind: string; // e.g. "author", "reviewer", e.t.c
+  }>;
+  meta: unknown; // could be anything
+}
+
+// End of duplicated code
+
 // This is the endpoint that does the actual token exchange/user lookup and signing the output token
 // And yeah, I know the controller/usecase code shouldn't be mixed but idec, we can refactor it at some point
 // The tokens will take the following shape:
-const example_token = {
-  iss: "journal--prod",
-  iat: 1567503944,
-  exp: 1567504004,
-  id: "jfrdocq8",
-  "new-session": true
-};
+// const example_token = {
+//   iss: "journal--prod",
+//   iat: 1567503944,
+//   exp: 1567504004,
+//   id: "jfrdocq8",
+//   "new-session": true
+// };
 
 export const Authenticate = (profilesService: ProfilesRepo) => (
   req: Request,
@@ -39,7 +64,26 @@ export const Authenticate = (profilesService: ProfilesRepo) => (
             logger.info("getProfile", profile);
             // TODO: Calculate user-role
 
-            const payload = { token_id: v4(), profile, user_roles: ["author"] };
+            const payload: UserIdentity = { 
+              token_id: v4(),
+              token_version: "0.1-alpha",
+              identity: {
+                user_id: v4(), // TODO: this needs to be a useful value at some point
+                external: [
+                  {
+                    id: profile.id,
+                    domain: "elife-profiles"
+                  },
+                  {
+                    id: profile.orcid,
+                    domain: "orcid",
+                  }
+                ]
+              },
+              roles: [{journal: "elife", kind: "author"}],
+              meta: null,
+            };
+
             const output_token = encode(payload);
 
             res.redirect(`${authorised_redirect_url}#${output_token}`);
--- continuum-auth/src/use-cases/index.ts
+++ continuum-auth/src/use-cases/index.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
+import { DomainLogger as logger } from '../logger';
 
 export * from "./authenticate";
 export * from "./login";
 
 export const HealthCheck = (deps: unknown) => (req: Request, res: Response) => {
+  logger.info("healthCheck");
   res.json({ ok: true });
 };
