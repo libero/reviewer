49001ef21a94266d0e15bac63ae1bdb6a00b8313
Cory <c.donkin@elifesciences.org>
2019-09-11 17:25:40 +0100

    begin forming checkbox component

---
+++ client/src/ui/stories/atoms/Checkbox.stories.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { storiesOf } from '@storybook/react';
+import { boolean, text, withKnobs } from '@storybook/addon-knobs';
+import centered from '@storybook/addon-centered/react';
+import { Checkbox } from '../../atoms';
+import '../../../core/styles/index.scss';
+
+const CheckboxComponent = () => {
+  const [ checked, setChecked ] = useState(false);
+  const label = text('Label', 'Check me?');
+  const inValid = boolean('Invalid', false);
+  const helperText = text('Helper Text', '');
+  return (
+      <Checkbox
+          id="someid"
+          onChange={() => setChecked(!checked)}
+          checked={checked}
+          invalid={inValid}
+          labelText={label}
+          helperText={helperText}
+      />
+  );
+}
+
+storiesOf('ui | atoms/Checkbox', module)
+    .addDecorator(withKnobs)
+    .addDecorator(centered)
+    .add(
+        'Checkbox',
+        (): JSX.Element => <CheckboxComponent />,
+    );
