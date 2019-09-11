267832b87e79ded6b76f086af3f6eaccfd24e57a
Peter East <petereast@users.noreply.github.com>
2019-09-11 10:15:17 +0100

    Prettier (#223)

--- message-bus-tester/src/mock-message-queue/index.ts
+++ message-bus-tester/src/mock-message-queue/index.ts
@@ -1,21 +1,30 @@
-import { EventPublisher, EventSubscriber, EventIdentifier, Event } from "../message-queue";
+import {
+  EventPublisher,
+  EventSubscriber,
+  EventIdentifier,
+  Event
+} from "../message-queue";
 import { InfraLogger as logger } from "../logger";
 import { Option, None, Some } from "funfix";
 
-export class MockMessageQueue implements EventPublisher, EventSubscriber{
-  private queues: Option<Map<string, (ev: Event<any>) => Promise<boolean>>> = None;
+export class MockMessageQueue implements EventPublisher, EventSubscriber {
+  private queues: Option<
+    Map<string, (ev: Event<any>) => Promise<boolean>>
+  > = None;
 
-  constructor() {
+  public async init(
+    defs: EventIdentifier[],
+    _service_name: string
+  ): Promise<this> {
     this.queues = Some(new Map());
-  }
-
-  public async init(defs: EventIdentifier[], _service_name: string): Promise<this> {
-    // Create queues in the map
     return this;
   }
 
   public async publish<T extends object>(event: Event<T>): Promise<boolean> {
-    return Option.of(this.queues.get().get(`${event.namespace}-${event.kind}`))
+    return this.queues
+      .flatMap(queues =>
+        Option.of(queues.get(`${event.namespace}-${event.kind}`))
+      )
       .map(fn => {
         return fn(event);
       })
