8f04ae9463383c51612b6b25b5ecf055253c9454
Peter East <petereast+gpg@pm.me>
2019-09-12 11:57:28 +0100

    Fix lint in shared libs

--- lib/event-bus/src/logger.ts
+++ lib/event-bus/src/logger.ts
@@ -1,10 +1,10 @@
-import * as pino from "pino";
+import * as pino from 'pino';
 
 const Logger = pino();
 
-const ServiceLogger = Logger.child({ service: "event-bus" });
+const ServiceLogger = Logger.child({ service: 'event-bus' });
 
-export const InfraLogger = ServiceLogger.child({ realm: "infra" });
-export const DomainLogger = ServiceLogger.child({ realm: "domain" });
+export const InfraLogger = ServiceLogger.child({ realm: 'infra' });
+export const DomainLogger = ServiceLogger.child({ realm: 'domain' });
 
 export default Logger;
