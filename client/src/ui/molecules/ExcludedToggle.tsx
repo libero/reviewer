8c4094da60fb188d1b03180dc3838ac91475f8e8
Cory <c.donkin@elifesciences.org>
2019-09-17 10:35:54 +0100

    add ExcludedToggle component

---
+++ client/src/ui/molecules/ExcludedToggle.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode, Fragment, useState } from 'react';
+import Close from '@material-ui/icons/Close';
+interface Props {
+    children?: ReactNode;
+    panelHeading?: string;
+    togglePrefixText: string;
+    toggleActionText: string;
+    open?: boolean;
+}
+
+const ExcludedToggle = ({ open = false, togglePrefixText, toggleActionText, children, panelHeading }: Props): JSX.Element => {
+    const [opened, setOpened] = useState(open);
+    return (
+        <Fragment>
+            {opened ? (
+                <div className="excluded-toggle__panel">
+                  <div className="excluded-toggle__panel-header">
+                    <h3 className="excluded-toggle__panel-heading">{panelHeading}</h3>
+                    <button className="excluded-toggle__close-button" onClick={(): void => setOpened(!opened)}><Close /></button>
+                  </div>
+                  {children}
+                </div>
+            ) : (
+                <span className="excluded-toggle__label">
+                    {togglePrefixText}
+                    <button className="excluded-toggle__action" aria-expanded={opened} onClick={(): void => setOpened(!opened)}>
+                        {toggleActionText}
+                    </button>
+                    ?
+                </span>
+            )}
+        </Fragment>
+    );
+};
+
+export default ExcludedToggle;
---
+++ client/src/ui/stories/molecules/ExcludedToggle.stories.tsx
@@ -0,0 +1,34 @@
+import React, { Fragment } from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, withKnobs, boolean, number } from '@storybook/addon-knobs';
+import { TextField } from '../../atoms';
+import { ExcludedToggle } from '../../molecules';
+import '../../../core/styles/index.scss';
+import centered from '@storybook/addon-centered/react';
+
+storiesOf('ui | molecules/ExcludedToggle', module)
+    .addDecorator(withKnobs)
+    .addDecorator(centered)
+    .add(
+        'ExcludedToggle',
+        (): JSX.Element => {
+            const togglePrefixText = text('Toggle prefix text', 'Would you like to ');
+            const toggleActionText = text('Toggle action text', 'exclude a senior editor');
+            const panelHeading = text('Panel heading', 'Excluded a senior editor');
+            const toggleContent = boolean('Display inner content', false);
+            const containerWidth = number('Container width', 600)
+            return (
+                <div style={{width: `${containerWidth}px`}}>
+                  <ExcludedToggle togglePrefixText={togglePrefixText} toggleActionText={toggleActionText} panelHeading={panelHeading}>
+                      {toggleContent && (
+                          <Fragment>
+                              <TextField id="someInput1" labelText="Some Lablel" />
+                              <TextField id="someInput2" labelText="Some Lablel" />
+                              <TextField id="someInput3" labelText="Some Lablel" />
+                          </Fragment>
+                      )}
+                  </ExcludedToggle>
+                </div>
+            );
+        },
+    );
