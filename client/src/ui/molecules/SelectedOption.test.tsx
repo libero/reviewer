fb0db0822fb734a90795336e64058bc2e37e3949
erezmus <e.odier@elifesciences.org>
2019-09-24 09:31:36 +0100

    For #291: Added SelectedOption component (#302)

---
+++ client/src/ui/molecules/SelectedOption.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, RenderResult } from '@testing-library/react';
+import { SelectedOption } from './SelectedOption';
+
+describe('SelectedOption', (): void => {
+    it('should render correctly', (): void => {
+        expect((): RenderResult => render(<SelectedOption text="Ian Malcolm" />)).not.toThrow();
+    });
+
+    it('should render the option text', (): void => {
+        const { container } = render(<SelectedOption text="Ian Malcolm" />);
+
+        expect(container.querySelector('.selected-option__text').textContent).toBe('Ian Malcolm');
+    });
+})
