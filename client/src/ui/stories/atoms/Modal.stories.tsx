4b713f304ba1463bd1e8b2acf904eb0f8588a339
Will Byrne <w.byrne@elifesciences.org>
2019-09-24 12:23:35 +0100

    update the modal overlay and component to allow fullscreen

--- client/src/ui/stories/atoms/Modal.stories.tsx
+++ client/src/ui/stories/atoms/Modal.stories.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
-import { boolean, text, withKnobs } from '@storybook/addon-knobs';
+import { boolean, select, text, withKnobs } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import { Button, Modal } from '../../atoms';
 import '../../../core/styles/index.scss';
@@ -9,10 +9,20 @@ import useModal from '../../hooks/useModal';
 const ModalWithButton = (): JSX.Element => {
     const { isShowing, toggle } = useModal();
     const modalMessage = text('Modal Message', 'This is a modal');
+    const fullscreen = boolean('Fullscreen', false);
+    const buttonType = select('Button Type', ['primary', 'danger'], 'primary');
+    const buttonText = text('Button Text', 'Accept');
     return (
         <Fragment>
             <Button onClick={(): void => toggle()}>Show Modal</Button>
-            <Modal hide={toggle} isShowing={isShowing} onAccept={action('accept')}>
+            <Modal
+                hide={toggle}
+                isShowing={isShowing}
+                onAccept={action('accept')}
+                fullscreen={fullscreen}
+                buttonType={buttonType}
+                buttonText={buttonText}
+            >
                 <p>{modalMessage}</p>
             </Modal>
         </Fragment>
@@ -25,9 +35,19 @@ storiesOf('ui | atoms/Modal', module)
         'Modal',
         (): JSX.Element => {
             const isShowing = boolean('isShowing', false);
+            const fullscreen = boolean('Fullscreen', false);
             const modalMessage = text('Modal Message', 'This is a modal');
+            const buttonType = select('Button Type', ['primary', 'danger'], 'primary');
+            const buttonText = text('Button Text', 'Accept');
             return (
-                <Modal hide={action('hide')} isShowing={isShowing} onAccept={action('accept')}>
+                <Modal
+                    hide={action('hide')}
+                    isShowing={isShowing}
+                    onAccept={action('accept')}
+                    buttonType={buttonType}
+                    fullscreen={fullscreen}
+                    buttonText={buttonText}
+                >
                     <p>{modalMessage}</p>
                 </Modal>
             );
