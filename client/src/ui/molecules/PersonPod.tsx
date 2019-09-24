76bc3ea2cf066c17751a0b125e5b29ddaf924396
Cory <c.donkin@elifesciences.org>
2019-09-24 11:55:36 +0100

    Add person pod props to pass down

--- client/src/ui/molecules/PersonPod.tsx
+++ client/src/ui/molecules/PersonPod.tsx
@@ -1,18 +1,22 @@
 import React, { useState, MouseEvent } from 'react';
 import { Pod } from '../atoms';
-import { Add, CheckCircle, Info } from '@material-ui/icons';
+import { Add, CheckCircle, Info, TagFacesSharp } from '@material-ui/icons';
 
 interface Props {
     initialySelected?: boolean;
     toggleHandler(event: MouseEvent): void;
+    name?: string;
+    institution?: string;
+    tags?: [string?];
 }
 
-const PersonPod = ({ initialySelected, toggleHandler }: Props): JSX.Element => {
+const PersonPod = ({ initialySelected, toggleHandler, institution, name, tags = [] }: Props): JSX.Element => {
     const [selected, setSelected] = useState(initialySelected || false);
     const onClick = (event: MouseEvent): void => {
         setSelected(!selected);
         toggleHandler(event);
     };
+    const commaSeperatedTags = tags.join(',');
     return (
         <Pod
             buttonIcon={
@@ -26,14 +30,14 @@ const PersonPod = ({ initialySelected, toggleHandler }: Props): JSX.Element => {
             onClick={onClick}
         >
             <div className="person-pod__text">
-                <span className="typography__body typography__body--primary typography__body--no-margin">Bob Ross</span>
+                <span className="typography__body typography__body--primary typography__body--no-margin">{name}</span>
                 <span className="typography__small typography__small--primary typography__small--no-margin">
-                    Little Happy Trees
+                    { institution }
                 </span>
                 <div className="person-pod__inline_text">
                     <Info fontSize="small" />
                     <span className="typography__small typography__small--secondary typography__small--no-margin">
-                        Happy, Little, Trees
+                        { commaSeperatedTags }
                     </span>
                 </div>
             </div>
