246925bf5ec7d6782e8c4c74cc5922aeea8bceb2
Erez Odier <erez.odier@gmail.com>
2019-10-08 14:29:05 +0100

    connection tests for amqp connector

--- lib/event-bus/src/rabbit-event-bus/amqp-connector.test.ts
+++ lib/event-bus/src/rabbit-event-bus/amqp-connector.test.ts
@@ -1,3 +1,9 @@
+import { connect, Connection } from 'amqplib';
+import * as flushPromises from 'flush-promises';
+import AMQPConnector from "./amqp-connector";
+import { channel } from 'rs-channel-node';
+import { StateChange } from './types';
+
 // Tests for the amqp connector
 // - publishes to the right exchange
 // - subscribes to the right queue
@@ -9,3 +15,37 @@
 // Tests for integration of amqp connector and index
 
 // Test against running amqp instance - this will be when it's it's own repository
+
+jest.mock('amqplib');
+
+describe('AMQP connector', () => {
+    const url = 'http://example.com';
+
+    it('should connect properly and create a channel', async () => {
+        const mockConnection = {
+            createChannel: jest.fn(),
+            on: jest.fn()
+        } as unknown as Connection;
+
+        (connect as any).mockImplementation(async (): Promise<Connection> => mockConnection);
+        new AMQPConnector<{}>(url, channel<StateChange<{}>>(), [], [], 'service');
+
+        await flushPromises();
+        expect(connect).toHaveBeenCalledTimes(1);
+        expect(connect).toHaveBeenCalledWith(url);
+        expect(mockConnection.createChannel).toHaveBeenCalledTimes(1);
+    });
+
+    it.only('should throw an error when connection fails', async () => {
+        (connect as any).mockImplementation(async (): Promise<Connection> => Promise.reject());
+        // mock this somehow
+        const sender = (___: StateChange<{}>) => jest.fn();
+        const receiver = async (): Promise<StateChange<{}>> => { return {} as StateChange<{}>};
+
+        const connector = new AMQPConnector<{}>(url, [sender, receiver], [], [], 'service');
+
+        await flushPromises();
+        // expect(sender).toHaveBeenCalledTimes(1);
+        // expect(sender).toHaveBeenCalledWith({ newState: 'NOT_CONNECTED' });
+    });
+});
