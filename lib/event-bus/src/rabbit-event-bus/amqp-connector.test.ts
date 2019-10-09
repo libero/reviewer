71914cacc579c4de664da05361c0cefa47f2ae72
Erez Odier <erez.odier@gmail.com>
2019-10-09 11:43:40 +0100

    added more tests for exchanges

--- lib/event-bus/src/rabbit-event-bus/amqp-connector.test.ts
+++ lib/event-bus/src/rabbit-event-bus/amqp-connector.test.ts
@@ -1,8 +1,9 @@
 import { connect, Connection } from 'amqplib';
 import * as flushPromises from 'flush-promises';
 import AMQPConnector from './amqp-connector';
-import { channel } from 'rs-channel-node';
 import { StateChange } from './types';
+import { channel } from 'rs-channel-node';
+import { Event } from 'event-bus';
 
 // Tests for the amqp connector
 // - publishes to the right exchange
@@ -52,4 +53,64 @@ describe('AMQP connector', () => {
         expect(sender).toHaveBeenCalledTimes(1);
         expect(sender).toHaveBeenCalledWith({ newState: 'NOT_CONNECTED' });
     });
+
+    it('should assert the right exchanges', async () => {
+        const mockChannel = {
+            assertExchange: jest.fn()
+        };
+        const mockConnection = {
+            createChannel: () => mockChannel,
+            on: jest.fn(),
+        } as unknown as Connection;
+
+        // tslint:disable-next-line
+        (connect as any).mockImplementation(async (): Promise<Connection> => mockConnection);
+        const _ = new AMQPConnector<{}>(url, channel(), [{ kind: 'foo', namespace: 'bar' }], [], 'service');
+
+        await flushPromises();
+
+        expect(mockChannel.assertExchange).toHaveBeenCalledTimes(1);
+        expect(mockChannel.assertExchange).toHaveBeenCalledWith('event__foo-bar', 'fanout');
+    })
+
+    it('should publish to the right exchanges', async () => {
+        const mockChannel = {
+            assertExchange: jest.fn(),
+            publish: jest.fn(),
+            on: jest.fn(),
+            close: jest.fn(),
+        };
+        const mockConnection = {
+            createChannel: () => mockChannel,
+            on: jest.fn(),
+        } as unknown as Connection;
+        const event = {
+            kind: 'foo',
+            namespace: 'bar',
+            id: 'id',
+            created: new Date(),
+            payload: { data: 'payload' }
+        };
+
+        // tslint:disable-next-line
+        (connect as any).mockImplementation(async (): Promise<Connection> => mockConnection);
+        const connector = new AMQPConnector<{}>(url, channel(), [{ kind: 'foo', namespace: 'bar' }], [], 'service');
+
+        // we need to wait for connection to be stored before we can publish
+        await flushPromises();
+        await connector.publish(event as Event<{}>);
+
+        await flushPromises();
+        expect(mockChannel.publish).toHaveBeenCalledTimes(1);
+        expect(mockChannel.publish.mock.calls[0][0]).toBe('event__foo-bar');
+        expect(mockChannel.publish.mock.calls[0][1]).toBe('');
+        expect(mockChannel.publish.mock.calls[0][2]).toEqual(Buffer.from(JSON.stringify({
+            event,
+            meta: {
+                attempts: 0,
+                retries: 10,
+                failures: 0, // increments each failure
+            }
+        })));
+    })
 });
