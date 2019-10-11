1ab868c81712b94361a4be3ed38fc8196967c7ca
nlisgo <nathan@aeopus.com>
2019-10-11 14:06:15 +0100

    Setup linting and address exisitng cs violations

--- audit/src/events.ts
+++ audit/src/events.ts
@@ -1,23 +1,23 @@
-import { Event, EventIdentifier } from "@libero/event-bus";
+import { Event, EventIdentifier } from '@libero/event-bus';
 
-export type ServiceStartedPayload = {
+export interface ServiceStartedPayload {
   name: string;
   type: string;
-};
+}
 
 export const serviceStartedIdentifier: EventIdentifier = {
-  kind: "ServiceStarted",
-  namespace: "infra-global"
+  kind: 'ServiceStarted',
+  namespace: 'infra-global',
 };
 
-export type UserLoggedInPayload = {
+export interface UserLoggedInPayload {
   name: string;
   userId: string;
   email: string;
   timestamp: Date;
-};
+}
 
 export const userLoggedInIdentifier: EventIdentifier = {
-  kind: "UserLoggedIn",
-  namespace: "user-audit",
+  kind: 'UserLoggedIn',
+  namespace: 'user-audit',
 };
--- audit/src/handlers/index.ts
+++ audit/src/handlers/index.ts
@@ -1,7 +1,7 @@
 // Event handlers - returns
-import { Event } from "@libero/event-bus";
-import { ServiceStartedPayload } from "../events";
-import { InfraLogger as logger } from "../logger";
+import { Event } from '@libero/event-bus';
+import { ServiceStartedPayload } from '../events';
+import { InfraLogger as logger } from '../logger';
 import { AuditController } from '../domain/audit';
 import { AuditLogItem} from '../domain/types';
 import { v4 } from 'uuid';
@@ -11,17 +11,17 @@ export type EventHandler<T extends object> = (
 ) => (ev: Event<T>) => Promise<boolean>;
 
 export const ServiceStartedHandler = (auditDomain: AuditController) => async (
-  ev: Event<ServiceStartedPayload>
+  ev: Event<ServiceStartedPayload>,
 ) => {
   // Transform the event payload into an auditable event
 
   const auditItem: AuditLogItem = {
     id: v4(),
     subject: `${ev.payload.name}-${ev.payload.type}`,
-    verb: "STARTED",
-    entity: "NONE",
-  }
+    verb: 'STARTED',
+    entity: 'NONE',
+  };
 
-  logger.info("serviceStartedRecieved", ev.payload);
+  logger.info('serviceStartedRecieved', ev.payload);
   return await auditDomain.recordAudit(auditItem);
 };
