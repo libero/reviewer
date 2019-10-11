67a2599835f475751d4878620b8d9c4a6e51ccc3
nlisgo <nathan@aeopus.com>
2019-10-11 10:58:38 +0100

    Address cs issues in message-bus-tester

--- message-bus-tester/src/main.ts
+++ message-bus-tester/src/main.ts
@@ -1,24 +1,24 @@
-import * as express from "express";
-import { Request, Response } from "express";
+import * as express from 'express';
+import { Request, Response } from 'express';
 import { InfraLogger as logger } from './logger';
 import { Event, EventIdentifier, RabbitEventBus, MockEventBus } from '@libero/event-bus';
 
-type TestEventPayload = {
+interface TestEventPayload {
   x: number;
   y: number;
-};
+}
 
-const init_mq = async () => {
-  const test_event_def: EventIdentifier = {
-    kind: "something_else",
-    namespace: "service_01",
+const initMq = async () => {
+  const testEventDef: EventIdentifier = {
+    kind: 'something_else',
+    namespace: 'service_01',
   };
 
   // Mock bus
-  const mock_mq = await (new MockEventBus()).init([test_event_def, test_event_def], "message-bus-test");
-  logger.info("messageQueueStarted");
+  const mockMq = await (new MockEventBus()).init([testEventDef, testEventDef], 'message-bus-test');
+  logger.info('messageQueueStarted');
 
-  mock_mq.subscribe<TestEventPayload>(test_event_def, async (event) => {
+  mockMq.subscribe<TestEventPayload>(testEventDef, async (event) => {
     logger.info('mockEventReceived', event);
     return true;
   });
@@ -31,17 +31,17 @@ const init_mq = async () => {
         x: 10,
         y: 20,
       },
-      ...test_event_def
-    }
+      ...testEventDef,
+    };
 
-    mock_mq.publish(event);
-  }, 10000)
+    mockMq.publish(event);
+  }, 10000);
 
   // Rabbit bus
-  const rabbitmq_mq = await (new RabbitEventBus({url: 'amqp://rabbitmq'})).init([test_event_def, test_event_def], "message-bus-test");
-  logger.info("messageQueueStarted");
+  const rabbitmqMq = await (new RabbitEventBus({url: 'amqp://rabbitmq'})).init([testEventDef, testEventDef], 'message-bus-test');
+  logger.info('messageQueueStarted');
 
-  rabbitmq_mq.subscribe<TestEventPayload>(test_event_def, async (event: Event<TestEventPayload>) => {
+  rabbitmqMq.subscribe<TestEventPayload>(testEventDef, async (event: Event<TestEventPayload>) => {
     logger.info('rabbitEventRecieved', event);
     return true;
   });
@@ -54,30 +54,29 @@ const init_mq = async () => {
         x: 10,
         y: 20,
       },
-      ...test_event_def
-    }
+      ...testEventDef,
+    };
 
-    console.log("send attempt");
-    rabbitmq_mq.publish(event).then(() => console.log("send complete"));
-  }, 500)
+    rabbitmqMq.publish(event);
+  }, 500);
 
 };
 
 const main = () => {
-  init_mq();
+  initMq();
   const app = express();
-  app.use("/", (req: Request, res: Response, next) => {
+  app.use('/', (req: Request, res: Response, next) => {
     // Maybe this should be trace level logging
     logger.info(`${req.method} ${req.path}`, {});
     next();
   });
 
-  app.get("/health", (req: Request, res: Response) => {
-    logger.info("/healthcheck");
-    res.json({ ok: true })
+  app.get('/health', (req: Request, res: Response) => {
+    logger.info('/healthcheck');
+    res.json({ ok: true });
   });
 
   return app;
 };
 
-main().listen(3002, () => logger.info("applicationStartup"));
+main().listen(3002, () => logger.info('applicationStartup'));
