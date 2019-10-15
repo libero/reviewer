680cec1a4cdaf828778a12355f42616b7e45d7cc
Peter Hooper <diversemix@gmail.com>
2019-10-15 15:27:23 +0100

    Tidy and expand tests

--- lib/event-bus/src/mock-event-bus/index.test.ts
+++ lib/event-bus/src/mock-event-bus/index.test.ts
@@ -1,37 +1,103 @@
-import { MockEventBus } from '.';
-import { EventType, Event } from '../event-bus';
+import { MockEventBus } from ".";
+import { EventType, Event } from "../event-bus";
 
-interface TestEventPayload {
+interface TestEventPayload1 {
   x: number;
   y: number;
 }
 
-describe('mock message queue', () => {
-  describe('you can publish and subscribe', () => {
-    it('can do the full flow', async () => {
-      const eventType: EventType = 'libero:mock:test';
+interface TestEventPayload2 {
+  a: number;
+  b: number;
+}
 
-      const handlerMock = jest.fn(async () => true);
+type MockEvent1 = Event<TestEventPayload1>;
+type MockEvent2 = Event<TestEventPayload2>;
 
-      // Mock bus
-      const mockMq = await (new MockEventBus()).init([eventType, eventType], 'message-bus-test');
+describe("mock message queue", () => {
+  describe("you can publish and subscribe", () => {
+    it("can do the full flow", async () => {
+      const eventType: EventType = "libero:mock:test";
 
-      mockMq.subscribe<TestEventPayload>(eventType, handlerMock);
+      const mockHandler = jest.fn(async () => true);
+      const mockEventBus = await new MockEventBus().init(
+        [eventType],
+        "message-bus-test"
+      );
 
-      const event: Event<TestEventPayload> = {
+      mockEventBus.subscribe<MockEvent1>(eventType, mockHandler);
+
+      const event: Event<TestEventPayload1> = {
         eventType,
-        id: 'some-testing-event-id',
+        id: "some-testing-event-id",
         created: new Date(),
-        payload: {
-          x: 10,
-          y: 20,
-        },
+        payload: { x: 10, y: 20 }
       };
 
-      mockMq.publish(event);
+      mockEventBus.publish(event);
 
-      expect(handlerMock).toBeCalled();
-      expect(handlerMock.mock.calls).toEqual([[event]]);
+      expect(mockHandler).toBeCalled();
+      expect(mockHandler.mock.calls).toEqual([[event]]);
+    });
+
+    it("can discriminate based on event type", async () => {
+      const eventType1: EventType = "libero:mock:test1";
+      const eventType2: EventType = "libero:mock:test2";
+
+      const event1: MockEvent1 = {
+        eventType: eventType1,
+        id: "some-testing-event1-id",
+        created: new Date(),
+        payload: { x: 10, y: 20 }
+      };
+
+      const event2: MockEvent2 = {
+        eventType: eventType2,
+        id: "some-testing-event2-id",
+        created: new Date(),
+        payload: { a: 10, b: 20 }
+      };
+
+      let handler1 = 0;
+      let handler2 = 0;
+      let receivedEvent1: object = {};
+      let receivedEvent2: object = {};
+
+      const mockHandler1 = async (event: Event<TestEventPayload1>) => {
+        handler1 += 1;
+        receivedEvent1 = event;
+        return Promise.resolve(true);
+      };
+
+      const mockHandler2 = async (event: Event<TestEventPayload2>) => {
+        handler2 += 1;
+        receivedEvent2 = event;
+        return Promise.resolve(true);
+      };
+
+      const mockEventBus = await new MockEventBus().init(
+        [eventType1, eventType2],
+        "message-bus-test"
+      );
+
+      mockEventBus.subscribe<TestEventPayload1>(eventType1, mockHandler1);
+      mockEventBus.subscribe<TestEventPayload2>(eventType2, mockHandler2);
+
+      mockEventBus.publish<TestEventPayload2>(event2);
+
+      expect(handler1).toBe(0);
+      expect(handler2).toBe(1);
+      expect(receivedEvent1).toEqual({});
+      expect(receivedEvent2).toEqual(event2);
+
+      receivedEvent1 = {};
+      receivedEvent2 = {};
+      mockEventBus.publish<TestEventPayload1>(event1);
+
+      expect(handler1).toBe(1);
+      expect(handler2).toBe(1);
+      expect(receivedEvent1).toEqual(event1);
+      expect(receivedEvent2).toEqual({});
     });
   });
 });
--- lib/event-bus/src/mock-event-bus/index.ts
+++ lib/event-bus/src/mock-event-bus/index.ts
@@ -1,39 +1,56 @@
-import {
-  EventPublisher,
-  EventSubscriber,
-  EventType,
-  Event,
-} from '../event-bus';
-import { Option, None, Some } from 'funfix';
+import { EventBus, EventType, Event, EventHandler } from "../event-bus";
+import { Option, None, Some } from "funfix";
 
-export class MockEventBus implements EventPublisher, EventSubscriber {
-  private queues: Option<
-    Map<string, (ev: Event<object>) => Promise<boolean>>
-  > = None;
+export type AnyEvent = Event<object>;
+export type AnyHandler = (ev: AnyEvent) => Promise<boolean>;
 
-  public async init(
-    defs: EventType[],
-    serviceName: string,
-  ): Promise<this> {
+/**
+ * Mocks out the EventBus for use in tests.
+ *
+ * @export
+ * @class MockEventBus
+ * @implements {EventBus}
+ */
+export class MockEventBus implements EventBus {
+  private queues: Option<Map<string, AnyHandler>> = None;
+
+  public async init(defs: EventType[], serviceName: string): Promise<this> {
     this.queues = Some(new Map());
     return this;
   }
 
+  /**
+   * Allows the MockEventBus to publish any event of type Event<T>
+   *
+   * @template T
+   * @param {T} event
+   * @returns {Promise<boolean>}
+   * @memberof MockEventBus
+   */
   public async publish<T extends object>(event: Event<T>): Promise<boolean> {
+    console.log(`Publish ${JSON.stringify(event)}`);
+
     return this.queues
-      .flatMap(queues =>
-        Option.of(queues.get(`${event.eventType}`)),
-      )
+      .flatMap(queues => Option.of(queues.get(`${event.eventType}`)))
       .map(fn => {
         return fn(event);
       })
       .getOrElse(false);
   }
 
-  public async subscribe<P extends object>(
+  /**
+   * Allows the MockEventBus to subscribe any event of type Event<T>
+   *
+   * @template T
+   * @param {EventType} eventType
+   * @param {(event: T) => Promise<boolean>} handler
+   * @memberof MockEventBus
+   */
+  public async subscribe<T extends object>(
     eventType: EventType,
-    handler: (ev: Event<P>) => Promise<boolean>,
+    handler: (event: Event<T>) => Promise<boolean>
   ) {
+    console.log(`Subscribe ${eventType}`);
     this.queues.get().set(`${eventType}`, handler);
   }
 }
