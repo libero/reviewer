9b33ce4ff965961590b0b33cb0b2ef38d1902fa7
Erez Odier <erez.odier@gmail.com>
2019-10-17 12:53:26 +0100

    fix build issues by copying audit events for now

--- continuum-auth/src/use-cases/authenticate.test.ts
+++ continuum-auth/src/use-cases/authenticate.test.ts
@@ -4,7 +4,7 @@ import * as flushPromises from 'flush-promises';
 import { Authenticate } from './authenticate';
 import * as jwt from "../jwt";
 import config from '../config';
-import { userLoggedInIdentifier } from '../../../audit/src/events';
+import { userLoggedInIdentifier } from './audit-events';
 
 jest.mock('../logger');
 
