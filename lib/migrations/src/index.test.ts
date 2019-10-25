7d7d8fb2c950bb09a5dc7292a180c29ad8eee49d
nlisgo <nathan@aeopus.com>
2019-10-25 10:33:55 +0100

    Correct indentation

--- lib/migrations/src/index.test.ts
+++ lib/migrations/src/index.test.ts
@@ -2,19 +2,19 @@ import { Cli } from './index';
 import { Commands } from './commands';
 
 describe('cli', () => {
-  it('command init is called in the cli constructor', () => {
-    const options = {
-      name: 'name',
-      knexConfig: {
-        client: 'sqlite3',
-        useNullAsDefault: true,
-      },
-      migrations: {},
-    };
-    const init = Commands.prototype.init = jest.fn();
-    const commands = new Commands();
-    // tslint:disable-next-line: no-unused-expression
-    new Cli(options, commands);
-    expect(init).toHaveBeenCalledTimes(1);
-  });
+    it('command init is called in the cli constructor', () => {
+        const options = {
+            name: 'name',
+            knexConfig: {
+                client: 'sqlite3',
+                useNullAsDefault: true,
+            },
+            migrations: {},
+        };
+        const init = Commands.prototype.init = jest.fn();
+        const commands = new Commands();
+        // tslint:disable-next-line: no-unused-expression
+        new Cli(options, commands);
+        expect(init).toHaveBeenCalledTimes(1);
+    });
 });
