5de37b9b98ec4844ee2eda5d67b791f8e317960c
nlisgo <nathan@aeopus.com>
2019-10-25 15:01:40 +0100

    Indentation

--- lib/migrations/example/migrations/1571920190-test.ts
+++ lib/migrations/example/migrations/1571920190-test.ts
@@ -2,14 +2,14 @@
 import * as Knex from 'knex';
 
 export default {
-  up(knex: Knex) {
-    return knex.schema.createTable('posts', (table: Knex.TableBuilder) => {
-        table.bigIncrements('id');
-        table.string('title');
-        table.text('content');
-    });
-  },
-  down(knex: Knex) {
-      return knex.schema.dropTable('posts');
-  },
+    up(knex: Knex) {
+        return knex.schema.createTable('posts', (table: Knex.TableBuilder) => {
+            table.bigIncrements('id');
+            table.string('title');
+            table.text('content');
+        });
+    },
+    down(knex: Knex) {
+        return knex.schema.dropTable('posts');
+    },
 };
--- lib/migrations/src/migration-template.ts
+++ lib/migrations/src/migration-template.ts
@@ -1,12 +1,12 @@
 export default  `import * as Knex from 'knex';
 
 export default {
-  up(knex: Knex) {
-    // create tables
-  },
+    up(knex: Knex) {
+        // create tables
+    },
 
-  down(knex: Knex) {
-    // drop tables
-  },
+    down(knex: Knex) {
+        // drop tables
+    },
 };
 `;
