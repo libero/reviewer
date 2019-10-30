c1ff1dfa5069232b9262c1d0db7ca0ae2d8668fa
nlisgo <nathan@aeopus.com>
2019-10-30 08:21:59 +0000

    cs

--- continuum-auth/src/config.ts
+++ continuum-auth/src/config.ts
@@ -1,29 +1,29 @@
 import { EventConfig } from '@libero/event-bus';
 
-const eventConfig : EventConfig = {
-  url: process.env.EVENTBUS_URL as string,
+const eventConfig: EventConfig = {
+    url: process.env.EVENTBUS_URL as string,
 };
 
 // This file will be responsible for loading the config from wherever it'll come from
 const config = {
-  auth: {
-    // Where the /login route sends you - a.k.a the identity server
-    login_redirect_url: `${process.env.CONTINUUM_LOGIN_URL}:${process.env.CONTINUUM_LOGIN_PORT}/submit`,
-    // App entry point i.e. the resource server that needs authentication
-    authorised_redirect_url: `http://localhost:${process.env.CLIENT_PORT}/login`
-  },
-  internal_jwt: {
-    // This token is global to libero services
-    secret: process.env.AUTHENTICATION_JWT_SECRET as string,
-    expiresIn: "30m"
-  },
-  journal_jwt: {
-    // This secret is used by journal to sign outgoing tokens, and used here to verify those
-    // tokens
-    secret: process.env.CONTINUUM_LOGIN_JWT_SECRET as string
-  },
-  port: process.env.AUTHENTICATION_PORT || 3001,
-  event: eventConfig,
+    auth: {
+        // Where the /login route sends you - a.k.a the identity server
+        login_redirect_url: `${process.env.CONTINUUM_LOGIN_URL}:${process.env.CONTINUUM_LOGIN_PORT}/submit`,
+        // App entry point i.e. the resource server that needs authentication
+        authorised_redirect_url: `http://localhost:${process.env.CLIENT_PORT}/login`,
+    },
+    internal_jwt: {
+        // This token is global to libero services
+        secret: process.env.AUTHENTICATION_JWT_SECRET as string,
+        expiresIn: '30m',
+    },
+    journal_jwt: {
+        // This secret is used by journal to sign outgoing tokens, and used here to verify those
+        // tokens
+        secret: process.env.CONTINUUM_LOGIN_JWT_SECRET as string,
+    },
+    port: process.env.AUTHENTICATION_PORT || 3001,
+    event: eventConfig,
 };
 
 export default config;
--- continuum-auth/src/event-bus.ts
+++ continuum-auth/src/event-bus.ts
@@ -1,8 +1,8 @@
-import { RabbitEventBus, EventConfig } from '@libero/event-bus';
+import { RabbitEventBus, EventConfig, EventBus } from '@libero/event-bus';
 import { LiberoEventType } from '@libero/libero-events';
 
-export const setupEventBus = async (config: EventConfig) => {
-    const url = `amqp://${ config.url }`;
-    const eventBus = await (new RabbitEventBus({url}).init([LiberoEventType.userLoggedInIdentifier], 'continuum-auth'));
+export const setupEventBus = async (config: EventConfig): Promise<EventBus> => {
+    const url = `amqp://${config.url}`;
+    const eventBus = await new RabbitEventBus({ url }).init([LiberoEventType.userLoggedInIdentifier], 'continuum-auth');
     return eventBus;
 };
