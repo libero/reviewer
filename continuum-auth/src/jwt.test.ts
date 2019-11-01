4fadcf3dda54f2800f1e7cc43e8fc456527de955
nlisgo <nathan@aeopus.com>
2019-11-01 12:19:20 +0000

    Placeholder for jwt tests

---
+++ continuum-auth/src/jwt.test.ts
@@ -0,0 +1,10 @@
+describe('jwt', () => {
+    describe('decodeJournalToken', () => {
+        it.skip('returns JournalAuthToken if token verified', () => {
+            
+        });
+        it.skip('returns None if token not verified', () => {
+            
+        });
+    });
+});
