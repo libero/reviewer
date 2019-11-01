fba46aa4ed96f5b0caa9f560e6eab4e73ee85b03
Erez Odier <erez.odier@gmail.com>
2019-11-01 17:13:16 +0000

    added test in case of error

--- continuum-auth/src/repo/profiles.test.ts
+++ continuum-auth/src/repo/profiles.test.ts
@@ -1,14 +1,30 @@
-import { ProfilesService } from './profiles';
 import fetch from 'node-fetch';
+import { None } from 'funfix';
+import { ProfilesService } from './profiles';
+
+const { Response } = jest.requireActual('node-fetch');
 
 jest.mock('node-fetch');
 jest.mock('../logger');
 
 describe('profiles', () => {
-    it('fetchs a profile by id', () => {
+    afterEach(jest.resetAllMocks);
+
+    it('fetchs a profile by id', async () => {
+        const response = new Response('', { status: 200 });
+        (fetch as jest.MockedFunction<typeof fetch>).mockReturnValue(Promise.resolve(response));
+
         const profilesService = new ProfilesService('http://profiles_service_url');
-        profilesService.getProfileById('profile-id');
+        await profilesService.getProfileById('profile-id');
         expect(fetch).toHaveBeenCalledTimes(1);
         expect(fetch).toHaveBeenCalledWith('http://profiles_service_url/profile-id');
+    });
+
+    it('returns none value in case of error', async () => {
+        const response = new Response('', { status: 400 });
+        (fetch as jest.MockedFunction<typeof fetch>).mockReturnValue(Promise.resolve(response));
+
+        const profilesService = new ProfilesService('http://profiles_service_url');
+        expect(await profilesService.getProfileById('profile-id')).toBe(None);
     });
 });
