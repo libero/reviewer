f3db72505fa58738df0e3fd0281524bbac7909e3
nlisgo <nathan@aeopus.com>
2019-11-04 13:00:51 +0000

    Implement tests for jwt

--- continuum-auth/src/jwt.test.ts
+++ continuum-auth/src/jwt.test.ts
@@ -1,6 +1,49 @@
+import { verify } from 'jsonwebtoken';
+import { decodeJournalToken } from './jwt';
+import { None } from 'funfix';
+import { InfraLogger as logger } from './logger';
+
+jest.mock('jsonwebtoken');
+jest.mock('./logger');
+jest.mock('./config', () => ({
+    default: {
+        journal_jwt: {
+            secret: 'secret',
+        },
+    },
+}));
+
 describe('jwt', () => {
     describe('decodeJournalToken', () => {
-        it.skip('returns JournalAuthToken if token verified', () => {});
-        it.skip('returns None if token not verified', () => {});
+        beforeEach(() => jest.resetAllMocks());
+
+        it('returns JournalAuthToken if token verified', () => {
+            (verify as jest.Mock).mockReturnValueOnce({
+                iss: 'iss',
+                iat: 1,
+                exp: 1,
+                id: 'id',
+                'new-session': true,
+            });
+            expect(decodeJournalToken('token').get()).toEqual({
+                iss: 'iss',
+                iat: 1,
+                exp: 1,
+                id: 'id',
+                'new-session': true,
+            });
+            expect(verify).toHaveBeenCalledTimes(1);
+            expect(verify).toHaveBeenCalledWith('token', 'secret');
+        });
+
+        it('returns None if token not verified', () => {
+            (verify as jest.Mock).mockImplementation(() => {
+                throw new Error('not verified');
+            });
+            expect(decodeJournalToken('token')).toBe(None);
+            expect(verify).toHaveBeenCalledWith('token', 'secret');
+            expect(logger.warn).toHaveBeenCalledTimes(1);
+            expect(logger.warn).toHaveBeenCalledWith('invalidJournalJwt');
+        });
     });
 });
