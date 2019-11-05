caee643c6fb6ea6b94418628ee9a9bacc7df83d5
Will Byrne <w.byrne@elifesciences.org>
2019-11-05 14:46:26 +0000

    fix linting

--- audit/src/handlers/index.test.ts
+++ audit/src/handlers/index.test.ts
@@ -1,37 +1,37 @@
-import { Event } from "@libero/event-bus";
-import { UserLoggedInHandler } from "./index";
-import { AuditController } from "../domain/audit";
-import { UserLoggedInPayload, LiberoEventType } from "@libero/event-types";
+import { Event } from '@libero/event-bus';
+import { UserLoggedInHandler } from './index';
+import { AuditController } from '../domain/audit';
+import { UserLoggedInPayload, LiberoEventType } from '@libero/event-types';
 
-jest.mock("../logger");
+jest.mock('../logger');
 
 beforeEach(jest.resetAllMocks);
 
 const recordAudit = jest.fn();
 const controller = ({
   auditRepo: null,
-  recordAudit
+  recordAudit,
 } as unknown) as AuditController;
 
 beforeEach(jest.resetAllMocks);
 
-describe("UserLoggedInHandler", () => {
-  it("records audit item", () => {
+describe('UserLoggedInHandler', () => {
+  it('records audit item', () => {
     const handler = UserLoggedInHandler(controller);
     const event: Event<UserLoggedInPayload> = {
-      id: "event-started-id",
+      id: 'event-started-id',
       created: new Date(),
       payload: {
-        name: "name",
-        userId: "userId",
-        email: "email",
+        name: 'name',
+        userId: 'userId',
+        email: 'email',
         timestamp: new Date(),
-        result: "authorized"
+        result: 'authorized',
       },
       context: {
-        source: "source"
+        source: 'source',
       },
-      eventType: LiberoEventType.userLoggedInIdentifier
+      eventType: LiberoEventType.userLoggedInIdentifier,
     };
 
     handler(event);
@@ -39,9 +39,9 @@ describe("UserLoggedInHandler", () => {
     const auditItem = recordAudit.mock.calls[0][0];
 
     expect(recordAudit).toHaveBeenCalledTimes(1);
-    expect(auditItem.entity).toBe("user:userId");
-    expect(auditItem.action).toBe("LOGGED_IN");
-    expect(auditItem.object).toBe("source");
-    expect(auditItem.result).toBe("authorized");
+    expect(auditItem.entity).toBe('user:userId');
+    expect(auditItem.action).toBe('LOGGED_IN');
+    expect(auditItem.object).toBe('source');
+    expect(auditItem.result).toBe('authorized');
   });
 });
