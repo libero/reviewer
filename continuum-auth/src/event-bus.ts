07b315c3be17fbc93ef8bcdbc0270ce39ab2e1f7
Will Byrne <w.byrne@elifesciences.org>
2019-11-08 11:48:29 +0000

    remove continuum auth

--- continuum-auth/src/event-bus.ts
+++
@@ -1,8 +0,0 @@
-import { RabbitEventBus, EventConfig, EventBus } from '@libero/event-bus';
-import { LiberoEventType } from '@libero/event-types';
-
-export const setupEventBus = async (config: EventConfig): Promise<EventBus> => {
-    const url = `amqp://${config.url}`;
-    const eventBus = await new RabbitEventBus({ url }).init([LiberoEventType.userLoggedInIdentifier], 'continuum-auth');
-    return eventBus;
-};
