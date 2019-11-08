a065cfa1b11d30474b6bebede49d4da5ac8a8dcb
erezmus <e.odier@elifesciences.org>
2019-11-08 14:12:57 +0000

    removed audit folder (#448)

--- audit/src/endpoints/index.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-
-export type Endpoint = () => (req: Request, res: Response) => void;
-
-export const HealthCheck: Endpoint = () => (req: Request, res: Response) => {
-  res.status(200).json({ ok: true });
-};
