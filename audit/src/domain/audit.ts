c109344fd0eb865bb070111af7e98cfac66268fc
Erez Odier <erez.odier@gmail.com>
2019-11-08 14:03:49 +0000

    For #448: removed audit folder

--- audit/src/domain/audit.ts
+++
@@ -1,9 +0,0 @@
-import { AuditRepository, AuditLogItem } from './types';
-
-export class AuditController {
-  public constructor(private readonly auditRepo: AuditRepository) {}
-
-  public async recordAudit(item: AuditLogItem): Promise<boolean> {
-    return this.auditRepo.putLog(item);
-  }
-}
