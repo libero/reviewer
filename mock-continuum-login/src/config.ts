443fc3e259eeb78f65415346d73a7d39278004bc
Will Byrne <w.byrne@elifesciences.org>
2019-11-08 16:46:05 +0000

    remove mock-continuum-auth (#475)

--- mock-continuum-login/src/config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    port: process.env.CONTINUUM_LOGIN_PORT,
-    authenticationUrl: `${process.env.AUTHENTICATION_URL}:${process.env.AUTHENTICATION_PORT}`,
-    continuumLoginJwtSecret: process.env.CONTINUUM_LOGIN_JWT_SECRET
-}
--- mock-continuum-login/src/use-cases/index.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-export * from "./submit";
-
-export const HealthCheck = (deps: unknown) => (req: Request, res: Response) => {
-  res.json({ ok: true });
-};
