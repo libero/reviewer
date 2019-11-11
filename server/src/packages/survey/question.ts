c587faf7d5db84fa4978f4a1d9e7ca551b18f98e
Peter Hooper <diversemix@gmail.com>
2019-11-11 13:50:57 +0000

    remove server folder

--- server/src/packages/survey/question.ts
+++
@@ -1,9 +0,0 @@
-export class Question {
-  readonly id: string;
-  readonly question: string;
-
-  constructor(id: string, question: string) {
-    this.id = id;
-    this.question = question;
-  }
-}
